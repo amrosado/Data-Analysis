06f14edcd591db6cac22d620ceef68a6e5180c5d
Aaron Rosado <aaron.rosado@gmail.com>
2016-01-11 15:44:22 -0500

    Beginning to write file parser.

--- BFPDataAnalysis/bfpDataParsing.py
+++ BFPDataAnalysis/bfpDataParsing.py
@@ -9,10 +9,71 @@ class BFPData:
     def parseFile(self):
         fileText = self.currentFile.read()
         fileLines = fileText.split('\n')
-        for line in fileLines:
+        expParaLines = fileLines[0:3]
+        expLines = fileLines[4:len(fileLines)]
+        expPara = {}
+        expData = []
+        startEvent = True
+        startTime = False
+        eventData = []
+        eventInfo = []
+        count = 0
+        firstExpParaLine = True
+        secondExpParaLine = False
+        thirdExpParaLine = False
+        for line in expParaLines:
             lineData = line.split('\t')
-            for data in lineData:
-                pass
+            if firstExpParaLine:
+                expPara['experimentMode'] = float(lineData[0])
+                expPara['edgesNumber'] = float(lineData[1])
+                expPara['u2ratio'] = float(lineData[2])
+                expPara['velocityBias'] = float(lineData[3])
+                firstExpParaLine = False
+                secondExpParaLine = True
+            elif secondExpParaLine:
+                expPara['springConstant'] = float(lineData[0])
+                expPara['pipetteDiameter'] = float(lineData[1])
+                expPara['rbcCellDiameter'] = float(lineData[2])
+                expPara['contactDiscDiameter'] = float(lineData[3])
+                expPara['beadDiameter'] = float(lineData[4])
+                expPara['aspiratedLength'] = float(lineData[5])
+                expPara['aspirationPressure'] = float(lineData[6])
+                expPara['temperature'] = float(lineData[7])
+                expPara['viscosity'] = float(lineData[8])
+                expPara['corticalTension'] = float(lineData[9])
+                secondExpParaLine = False
+                thirdExpParaLine = True
+            elif thirdExpParaLine:
+                expPara['impingingRate'] = float(lineData[0])
+                expPara['loadingRate'] = float(lineData[1])
+                expPara['primingRate'] = float(lineData[2])
+                expPara['retractingRate'] = float(lineData[3])
+                expPara['impingmentForce'] = float(lineData[4])
+                expPara['clampForce'] = float(lineData[5])
+                expPara['activationForce'] = float(lineData[6])
+                expPara['timeoutAtClamp'] = float(lineData[7])
+                expPara['contactTimeInSeconds'] = float(lineData[8])
+                expPara['cycleInterval'] = float(lineData[9])
+                firstExpParaLine = True
+                secondExpParaLine = False
+                thirdExpParaLine = False
+        for line in expLines:
+            lineData = line.split('\t')
+            if startEvent:
+                for data in lineData:
+                    eventInfo.append(float(data))
+                startEvent = False
+                startTime = True
+            else:
+                if startTime:
+                    for data in lineData:
+                        eventData.append(float(data))
+                else:
+                    if (0.000000 == eventInfo[0]) and (0.0000000 == eventInfo[1]):
+                        startEvent = True
+                        startTime = True
+                    for data in lineData:
+                        eventData.append(float(data))
 
     def parseFilesInDirectory(self):
         if self.currentDirectory != None:
